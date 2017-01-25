c5882460eb253fd11ecb2a18753fc1a8a517722c
Matheus <matheus-machado1@hotmail.com>
2017-01-25 14:18:22 -0200

    Exercicio

---
+++ AngularJs/Matheus/exercicio.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html ng-app>
+
+	<head>
+		<title></title>
+	</head>
+
+		<script src="../angular.min.js"></script>
+
+	<body>
+
+	</body>
+
+	<script>
+	angular.modile('app',[])
+	.controller 
+
+	</script>
+
+</html>>
